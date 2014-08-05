0335674188551329ae5fdb90403335d0bf17b870
Sharat M R <cosmosgenius@gmail.com>
2014-08-06 00:31:53 +0530

    added Form entry

--- src/main/scala/mib_desk/menus/RegistrationMenu.scala
+++ src/main/scala/mib_desk/menus/RegistrationMenu.scala
@@ -2,6 +2,13 @@ package mib_desk.menus
 
 object RegstrationMenu {
   def show(): Int = {
+    println( "\n\n\tRegistration Form" );
+    println( "\n\tCode Name provided to you?" );
+    println( "\tWhich Planet?" );
+    println( "\tColor of Blood?" );
+    println( "\tNumber of Antennas?" );
+    println( "\tNumber of Hands?" );
+    println( "\tNumber of Legs?" );
     
     return 0;
   }
