92c713c3097b3db8c18ba332ab602d786024f95f
Sharat M R <cosmosgenius@gmail.com>
2014-08-05 23:04:02 +0530

    Added initial text

--- src/main/scala/mib_desk/main.scala
+++ src/main/scala/mib_desk/main.scala
@@ -2,6 +2,7 @@ package mib_desk
 
 object Main {
   def main( args: Array[String] ): Unit = {
+    println("Welcome to MIB Registration Desk");
     for ( ln <- io.Source.stdin.getLines() ) {
       println( ln );
     }
