10399c66d8f2beba3c34b8e54280399871884eb6
Sharat M R <cosmosgenius@gmail.com>
2014-08-05 23:30:33 +0530

    added menu

--- src/main/scala/mib_desk/main.scala
+++ src/main/scala/mib_desk/main.scala
@@ -2,9 +2,33 @@ package mib_desk
 
 object Main {
   def main( args: Array[String] ): Unit = {
-    println("Welcome to MIB Registration Desk");
-    for ( ln <- io.Source.stdin.getLines() ) {
-      println( ln );
+
+    while ( processCmd( menu() ) ) {};
+  }
+
+  def menu(): Int = {
+    println( "\n\nWelcome to MIB Registration Desk" );
+    println( "\n\tWhat would you like to do?" );
+    println( "\t1. New Registration" );
+    println( "\t2. View Alien Details" );
+    println( "\t3. Exit" );
+
+    var optionStr = io.StdIn.readLine( "\t>> ", null );
+    var option = 0;
+
+    try {
+      option = optionStr.toInt;
+    } catch {
+      case e: NumberFormatException => println( "\n\tInvalid option!! Try again" );
+    }
+
+    return option;
+  }
+
+  def processCmd( option: Int ): Boolean = {
+    option match {
+      case 3 => return false;
+      case option => return true;
     }
   }
 }
