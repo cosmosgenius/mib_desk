497625c117be93cf10887a6951b7a2f99f348790
Sharat M R <cosmosgenius@gmail.com>
2014-08-06 16:20:48 +0530

    alienment correction

--- src/main/scala/mib_desk/formats/PDFFormat.scala
+++ src/main/scala/mib_desk/formats/PDFFormat.scala
@@ -2,7 +2,7 @@ package mib_desk.formats
 
 import mib_desk.bean.Alien
 
-class PDFFormat extends Format("PDF") {
+class PDFFormat extends Format( "PDF" ) {
   def save(): Boolean = {
     return true;
   }
